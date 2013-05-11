2c36218232325302f0415dbd276cf1e04261c340
Demolliens Olivier <olivier.demolliens@gmail.com>
2013-05-11 17:28:55 +0200

    fix git ignore (remove no needed resources)

--- build/Intermediates/ODASIHTTPRequest.build/Release-iphonesimulator/ODASIHTTPRequest-iOS.build/Script-012521DA14CDC9BC00E12214.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "BUILD DIRECTORY ${BUILD_DIR}"
-xcodebuild -project ${PROJECT_NAME}.xcodeproj -sdk iphonesimulator -target ${PROJECT_NAME} -configuration ${CONFIGURATION} clean build CONFIGURATION_BUILD_DIR=${BUILD_DIR}/${CONFIGURATION}-iphonesimulator
-
-xcodebuild -project ${PROJECT_NAME}.xcodeproj -sdk iphoneos -target ${PROJECT_NAME} -configuration ${CONFIGURATION} clean build CONFIGURATION_BUILD_DIR=${BUILD_DIR}/${CONFIGURATION}-iphoneos
-
